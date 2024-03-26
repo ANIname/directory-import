e67fd3cbca330dc48843a510602488ad996241aa
CaoMeiYouRen <996881204@qq.com>
2024-03-27 00:59:50 +0800

    fix: remove esm support because there is no `require` function in esm environments

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
+  format: ['cjs'],
   splitting: false,
   sourcemap: false,
   clean: true,
